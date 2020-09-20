271684ec8d6331cf5909479a1b747033b470f7af
Oguz Guzel <44399848+Oguz-Guzel@users.noreply.github.com>
2020-09-20 23:07:07 +0300

    Update network_complex.py

--- examples/complex/network_complex.py
+++ examples/complex/network_complex.py
@@ -1,22 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 13 23:48:57 2020
+
+@author: oguzguzel
+"""
+
 import tensorflow as tf
 from tensorflow import keras
 
 import pandas as pd
 import numpy as np
 
-train_df = pd.read_csv('./data/train.csv')
+# Load the data and shuffle each time
+train_df = pd.read_csv('../complex/data/train.csv')
 np.random.shuffle(train_df.values)
 
-print(train_df.head())
 
+# Building model around the data: Define an input layer of 2 neurons, 2 hidden layers of 32 neurons which has "reLU" activation and an output layer of 2 neurons
 model = keras.Sequential([
 	keras.layers.Dense(256, input_shape=(2,), activation='relu'),
-	keras.layers.Dropout(0.4),
-	keras.layers.Dense(128, activation='relu'),
-	keras.layers.Dropout(0.4),
-	keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dropout(0.4),
+    keras.layers.Dense(128,  activation='relu'),
+    keras.layers.Dropout(0.4),
+    keras.layers.Dense(128,  activation='relu'),
+    keras.layers.Dropout(0.4),
 	keras.layers.Dense(2, activation='sigmoid')])
 
+# Using "adam" optimizer 
 model.compile(optimizer='adam', 
 	          loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
 	          metrics=['accuracy'])
@@ -29,9 +40,6 @@ test_df = pd.read_csv('./data/test.csv')
 test_x = np.column_stack((test_df.x.values, test_df.y.values))
 
 print("EVALUATION")
+# Test the model
 model.evaluate(test_x, test_df.color.values)
-
-
-
-
 
